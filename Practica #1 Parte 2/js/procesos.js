d7ada6381a77eed35394a58528eb7e810dcb7809
AnisanYT <mejiadani07@gmail.com>
2023-10-07 11:49:17 -0600

    update7/10/23

--- Practica #1 Parte 2/js/procesos.js
+++ Practica #1 Parte 2/js/procesos.js
@@ -1,5 +1,12 @@
 var contador = 0;
-function aupdateSec(){ 
+var jsonArray = document.getElementById("json").value;
+var obtieneDatos = JSON.parse(jsonArray);
+var boton = 0;
+var win = 0;
+var dadosNUmero = document.getElementById("dadosNumero").getAttribute("data-value");
+//var casteo = JSON.parse(jsonArray);
+//var phpArray = eval("<?php echo json_encode($vectorAsociativo); ?>");
+function aupdateSec(){
     var fecha = new Date();
     document.getElementById("hora").innerText = fecha.getHours();
     document.getElementById("minutos").innerText = fecha.getMinutes();
@@ -12,4 +19,80 @@ function aupdateSec(){
     }
 }
 
+function actMonto() {
+    var monto = document.getElementById("montocito").getAttribute("data-value");
+    document.getElementById("monto").innerText = monto;
+    document.getElementById("monto").value = monto;
+}
+
+function resta(){
+    var montoActual = document.getElementById("montocito").getAttribute("data-value");
+    var montoApostado = document.getElementById("montoApostado").value;
+    var resta = montoActual - montoApostado;
+    parrafoDeMonto = document.getElementById("monto");
+    parrafoDeMonto.value = resta;
+    parrafoDeMonto.innerText = resta;
+}
+
+function asignaDadoInit() {
+    let numerodeDados = document.getElementById("dadosNumero").getAttribute("data-value");
+    if (parseInt(numerodeDados) === 2) {
+        document.getElementById("1").innerHTML = "<img src=\"" + obtieneDatos[1] + "\" heigth = \"200px\" width = \"200px\">";
+        document.getElementById("2").innerHTML = "<img src=\"" + obtieneDatos[2] + "\" heigth = \"200px\" width = \"200px\">";
+        document.getElementById("emojiSad_Happy").innerHTML = "<img src= \"img/interaccion/waitCat.jfif\" heigth = \"200px\" width = \"200px\">";
+    } else {
+        document.getElementById("1").innerHTML = "<img src=\"" + obtieneDatos[1] + "\" heigth = \"200px\" width = \"200px\">";
+        document.getElementById("2").innerHTML = "<img src=\"" + obtieneDatos[2] + "\" heigth = \"200px\" width = \"200px\">";
+        document.getElementById("3").innerHTML = "<img src=\"" + obtieneDatos[6] + "\" heigth = \"200px\" width = \"200px\">";
+        document.getElementById("emojiSad_Happy").innerHTML = "<img src= \"img/interaccion/waitCat.jfif\" heigth = \"200px\" width = \"200px\">";
+    }
+}
+
+function buildTheTable() {
+    var table = document.getElementById("buildTable");
+    var jsonTable = JSON.parse("../json/buildTable.json");
+    table.innerHTML = "../json/buildTable.json";
+}
+
+function jugar(){
+    var apostado = document.getElementById("montoApostado").value;
+    var montoDisponible = document.getElementById("monto").value;
+    imagenUno = document.getElementById("1");
+    imagenDos = document.getElementById("2");
+    imagenTres = document.getElementById("3");
+    var aux = 0;
+    if (apostado != "" && apostado < montoDisponible && boton != 0){
+        if (dadosNUmero == 2){
+            for (i = 0; i < dadosNUmero; i++) {
+                var numeroRandom = Math.floor(Math.random() * (6 - 1) + 1);
+                aux += numeroRandom;
+                if (i == 0){
+                    imagenUno.innerHTML = "<img src=\"" + obtieneDatos[numeroRandom] + "\" heigth = \"200px\" width = \"200px\">";
+                } else {
+                    imagenDos.innerHTML = "<img src=\"" + obtieneDatos[numeroRandom] + "\" heigth = \"200px\" width = \"200px\">";
+                }
+            }
+            if (boton == aux){
+                alert("Usted gano!");
+            } else {
+                resta();
+                document.getElementById("emojiSad_Happy").innerHTML = "<img src= \"img/interaccion/cryCat.png\" heigth = \"200px\" width = \"200px\">";
+                document.getElementById("numeroSeleccionado").innerText = "";
+                alert("Apostaste " + boton + " Pero cayò " + aux);
+                boton = 0;
+            }
+        }
+    
+    } else {
+        alert("Hmm, recuerda. No apuestas mayores del monto disponible y No dejes el espacio vacìo, ademas recuerda apostar un numero.");
+    }
+}
+
+function saveNum(event) {
+    boton = event.target.value;
+    document.getElementById("numeroSeleccionado").innerText = boton;
+}
 document.addEventListener("DOMContentLoaded", aupdateSec);
+document.addEventListener("DOMContentLoaded", actMonto);
+document.addEventListener("DOMContentLoaded", buildTheTable);
+document.addEventListener("DOMContentLoaded", asignaDadoInit);
--- Practica #1 Parte 2/validar.php
+++ Practica #1 Parte 2/validar.php
@@ -7,23 +7,67 @@
 </head>
 <body>
     <div class="container">
+    
         <?php
             $nombre = $_POST["nombre"];
             $monto = $_POST["monto"];
             $numDados = $_POST["dados"];
+            
+            $vectorAsociativo = array('1' => 'img\caraUno.png',
+                                      '2' => 'img\dosCara.png',
+                                      '3' => 'img\tresCara.png',
+                                      '4' => 'img\cuatroCara.png',
+                                      '5' => 'img\cincoCara.png',
+                                      '6' => 'img\seisCara.png');   
+            $jsonArray = json_encode($vectorAsociativo);
+            echo "<div id=\"montocito\" data-value=\"$monto\"></div>";
+            echo "<div id=\"dadosNumero\" data-value=\"$numDados\"></div>";
             #echo "<h1> Comprobar que todo esta bien: <br>" . $nombre . ", " . $monto . ", " . $numDados . "</h1>";
             echo "<h1 class=\"text-center\">Bienvenido " . $nombre . "</h1>";
 
             #contruimos la tabla para que se pueda ver los datos
             # Nombre | Daniel | Monto disponible | 200.000
             #                 | Monto apostado   | 10.000
-            echo "<table>";
+            echo "<div class = \"d-flex justify-content-center\"><table>";
             echo "<tr >";
             echo "<td class= \"border\" style=\"width: 100px; background: #cdbe8b\"> Nombre </td>";
             echo "<td class= \"border\" style=\"width: 400px; background: #fff0c2\">" . $nombre . "</td>";
+            echo "<td class= \"border\" style=\"width: 200px; background: #cdbe8b\"> Monto disponible </td>";
+            echo "<td class= text-center \"border\" style=\"width: 200px; background: #fff0c2\"> ₡" . "<p id=\"monto\" value=\"\"> </p>" . "</td>";
             echo "</tr>";
-            echo "</table>";
+            echo "<tr>";
+            echo "<td colspan=\"2\"></td>";
+            echo "<td class= \"border\" style=\"width: 200px; background: #cdbe8b\">Monto apostado</td>";
+            echo "<td class= text-center \"border\" style=\"width: 200px; background: #fff0c2\"> 
+            <input class=\"mx-2\" type=\"number\" name=\"montoApostado\" id=\"montoApostado\"></td>";
+            echo "</table></div>";
+            
+            #echo "<div class = \"d-flex justify-content-center\" onClick=\"resta()\"><button class = \"btn btn-success\"> Restar </button></div>";
+            echo "<div class =\"row m-4\">  ";
+            echo "<div id=\"1\"  class =\"col-3\" > </div>";
+            echo "<div id=\"2\"  class =\"col-3\" > </div>";
+            #echo "<img src=\"". $vectorAsociativo["1"] . "\">";
+            echo "<div id=\"3\"  class =\"col-3\" > </div>";
+            echo "<div id=\"emojiSad_Happy\" class =\"col-2\">  </div>";
+            echo "</div>";
+            echo "<button onClick=\"jugar()\" class = \"btn btn-success m-4\"> [ J U G A R] </button>";
+            echo "<div id= \"buildTable\"\>";
+            
+            echo "<h2 class= \"text-center\"> Elige un numero para apostar. </h2>";
+
+            if ($numDados == 2) {
+                include_once("segmentos/tabla2.inc");
+            } else {
+                include_once("segmentos/table3.inc");
+            }
+            echo "</div>";
+           
         ?> 
-    </div>       
+
+        <button type="text" style="background-color: transparent; border: none;" id="json" value='<?php echo $jsonArray; ?>'>   </button>
+        <script src="js/procesos.js"></script>
+    </div>  
+    
+         
 </body>
 </html>
